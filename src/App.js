fea4ed968e6103b092701efe0d782cbd314f150d
LindseyinTech <lindseyintech@gmail.com>
2021-12-23 13:31:11 -0800

    updating environment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./layout/Layout";
 
 function App() {
+  console.log(process.env)
   return (
     <div className="app-routes">
       <Switch>
